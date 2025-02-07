aca4c155d9f59d9318d3fb229e0161d35ca4e578
Carlos González <carlos.rgn@gmail.com>
2025-02-07 17:39:32 -0600

    no brace expansion

--- test.sh
+++ test.sh
@@ -3,9 +3,9 @@ set -e
 
 pnpm --filter "test-app-*" --filter "!test-app-express-cjs" exec sh -c "\
   rm -rf src/user && \
-  cp ../test-app-express-cjs/src/{app.module.ts,auth.guard.ts} ./src/ && \
+  cp ../test-app-express-cjs/src/app.module.ts ../test-app-express-cjs/src/auth.guard.ts ./src/ && \
   cp -r ../test-app-express-cjs/src/user ./src/ && \
-  cp ../test-app-express-cjs/test/{jest-e2e.json,app.e2e-spec.ts} ./test/ \
+  cp ../test-app-express-cjs/test/jest-e2e.json ../test-app-express-cjs/test/app.e2e-spec.ts ./test/ \
 "
 
 pnpm --filter "test-app-*" run test:e2e --no-cache
