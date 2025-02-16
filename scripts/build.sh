9430e3e98428b35411bade04fa4bb4409668efd9
Carlos González <carlos.rgn@gmail.com>
2025-02-16 16:31:56 -0600

    esm support

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,8 @@ set -e
 __dirname=$(dirname "$0")
 
 cd "$__dirname/../packages/nestjs-endpoints"
-pnpm run build
+rm -rf dist
+pnpm tsc --project tsconfig.cjs.json
+echo '{"type": "commonjs"}' > dist/cjs/package.json
+pnpm tsc --project tsconfig.esm.json && pnpm tsc-alias --project tsconfig.esm.json
+echo '{"type": "module"}' > dist/esm/package.json
