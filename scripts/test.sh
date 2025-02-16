2ae1ee3ef575f1b8196972540413681d822f9e41
Carlos González <carlos.rgn@gmail.com>
2025-02-15 20:46:20 -0600

    fix test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,9 +3,10 @@ set -e
 __dirname=$(dirname "$0")
 
 pnpm --filter "test-app-*" --filter "!test-app-express-cjs" exec sh -c "\
-  rm -rf src/user && \
   cp ../test-app-express-cjs/src/app.module.ts ../test-app-express-cjs/src/auth.guard.ts ./src/ && \
+  cp -r ../test-app-express-cjs/src/decorators ./src/ && \
   cp -r ../test-app-express-cjs/src/user ./src/ && \
+  cp -r ../test-app-express-cjs/src/test ./src/ && \
   cp ../test-app-express-cjs/test/jest-e2e.json ../test-app-express-cjs/test/app.e2e-spec.ts ./test/ \
 "
 
