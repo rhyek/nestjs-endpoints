4dd549b3b5f87394f9acfb6aa8f3e7ce59c68efd
Carlos González <carlos.rgn@gmail.com>
2025-03-01 14:46:00 -0600

    fix require from esm

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-__dirname=$(dirname "$0")
+__dirname=$(realpath "$(dirname "$0")")
 
 cd "$__dirname/../packages/nestjs-endpoints"
 rm -rf dist
@@ -8,3 +8,7 @@ pnpm tsc --project tsconfig.cjs.json
 echo '{"type": "commonjs"}' > dist/cjs/package.json
 pnpm tsc --project tsconfig.esm.json && pnpm tsc-alias --project tsconfig.esm.json
 echo '{"type": "module"}' > dist/esm/package.json
+# Modify the ESM router-module.js to use dynamic import instead of require
+sed -i.bak 's/require(f).default/await import(f).then((m) => m.default)/g' dist/esm/router-module.js
+# Remove backup file
+rm -f dist/esm/router-module.js.bak
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,22 +1,45 @@
 #!/bin/bash
 set -e
-__dirname=$(dirname "$0")
+__dirname=$(realpath "$(dirname "$0")")
 
 pnpm --filter "./packages/test-endpoints-module/test-app-express-cjs" run codegen
 
 pnpm --filter "./packages/test-endpoints-module/*" --filter "!test-app-express-cjs" exec sh -c "\
-  rsync -avr ../test-app-express-cjs/src/ ./src/ && \
-  rsync -avr ../test-app-express-cjs/generated/ ./generated/ && \
-  rsync -avr ../test-app-express-cjs/test/ ./test/ --exclude=create-app.ts
+  rsync -ar ../test-app-express-cjs/src/ ./src/ && \
+  rsync -ar ../test-app-express-cjs/generated/ ./generated/ && \
+  rsync -ar ../test-app-express-cjs/test/ ./test/ --exclude=create-app.ts
 "
 
 pnpm --filter "./packages/test-endpoints-router-module/*" --filter "!test-app-router-module-express-cjs" exec sh -c "\
-  rsync -avr ../test-app-express-cjs/src/ ./src/
+  rsync -ar ../test-app-express-cjs/src/ ./src/
 "
 
 pnpm --filter "./packages/test-endpoints-router-module/*" exec sh -c "\
-  rsync -avr ../../test-endpoints-module/test-app-express-cjs/generated/ ./generated/ && \
-  rsync -avr ../../test-endpoints-module/test-app-express-cjs/test/ ./test/ --exclude=create-app.ts
+  rsync -ar ../../test-endpoints-module/test-app-express-cjs/generated/ ./generated/ && \
+  rsync -ar ../../test-endpoints-module/test-app-express-cjs/test/ ./test/ --exclude=create-app.ts
 "
 
 pnpm --filter "test-app-*" run test:e2e --no-cache
+
+# Function to test a specific module type (cjs or esm)
+test_real_run() {
+  local type=$1
+  
+  cd $__dirname/../packages/test-endpoints-router-module/test-app-express-$type
+  # enable job control so we get a new process group for the server
+  set -m
+  pnpm start &
+  set +m
+  SERVER_PID=$!
+
+  wget -qO- https://raw.githubusercontent.com/eficode/wait-for/v2.2.3/wait-for | sh -s -- http://localhost:3000/test/status --timeout=5 -- echo $type success
+
+  kill -9 -- -$(ps -o pgid= -p $SERVER_PID)
+  wait $SERVER_PID 2>/dev/null || true
+}
+
+# Test CJS module
+test_real_run cjs
+
+# Test ESM module
+test_real_run esm
