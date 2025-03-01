93d0728c761a2d36614b0f7cef45bc846511d353
Carlos González <carlos.rgn@gmail.com>
2025-03-01 14:58:36 -0600

    SED_OPTS

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,4 +9,9 @@ echo '{"type": "commonjs"}' > dist/cjs/package.json
 pnpm tsc --project tsconfig.esm.json && pnpm tsc-alias --project tsconfig.esm.json
 echo '{"type": "module"}' > dist/esm/package.json
 # Modify the ESM router-module.js to use dynamic import instead of require
-sed -i '' 's/require(f).default/await import(f).then((m) => m.default)/g' dist/esm/router-module.js
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    SED_OPTS="-i ''"
+else
+    SED_OPTS="-i"
+fi
+sed $SED_OPTS 's/require(f).default/await import(f).then((m) => m.default)/g' dist/esm/router-module.js
